72d1bbae353af3d1f6640d43119a0a8e7c275b88
Martin Ro <martin-ro@users.noreply.github.com>
2023-01-14 14:11:51 +0800

    Dispatch NewStatus event when status changes

--- src/Filament/Resources/TicketResource/Pages/EditTicket.php
+++ src/Filament/Resources/TicketResource/Pages/EditTicket.php
@@ -5,10 +5,13 @@ namespace Sgcomptech\FilamentTicketing\Filament\Resources\TicketResource\Pages;
 use Filament\Pages\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use Sgcomptech\FilamentTicketing\Events\NewAssignment;
+use Sgcomptech\FilamentTicketing\Events\NewStatus;
 
 class EditTicket extends EditRecord
 {
     public $prev_assigned_to_id;
+
+    public $prev_status;
 
     public static function getResource(): string
     {
@@ -24,18 +27,23 @@ class EditTicket extends EditRecord
     {
         $interacted = $this->record?->ticketable;
 
-        return __('Ticket') . ' [' . $interacted?->{$interacted?->model_name()} . ']';
+        return __('Ticket') . ($interacted ? ' [' . $interacted?->{$interacted?->model_name()} . ']' : '');
     }
 
     protected function afterFill()
     {
         $this->prev_assigned_to_id = $this->record->assigned_to_id;
+        $this->prev_status = $this->record->status;
     }
 
     protected function afterSave()
     {
         if ($this->record->assigned_to_id != $this->prev_assigned_to_id) {
             NewAssignment::dispatch($this->record);
+        }
+
+        if ($this->record->status != $this->prev_status) {
+            NewStatus::dispatch($this->record);
         }
     }
 }
